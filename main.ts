215e3e271afbecb57ae1f6fbe4f0e51b6b81a2ce
HarryGrant34601 <hg4021@hw.ac.uk>
2025-10-23 12:27:18 +0100

    Removed testing blocks

--- main.ts
+++ main.ts
@@ -1,16 +1,18 @@
-let Score = 0
 function Start () {
     music.stopAllSounds()
-    Score = 34
-    EndGame()
+    // If end condition is met, it calls the end 
+    if (true) {
+        EndGame()
+    }
 }
 input.onButtonPressed(Button.AB, function () {
     Start()
 })
 function EndGame () {
+    let Score = 0
     basic.showString("Your Score was" + Score)
     music.setVolume(255)
-    music.play(music.tonePlayable(131, music.beat(BeatFraction.Whole)), music.PlaybackMode.LoopingInBackground)
+    music.play(music.tonePlayable(131, music.beat(BeatFraction.Breve)), music.PlaybackMode.LoopingInBackground)
 }
 basic.forever(function () {
 	
