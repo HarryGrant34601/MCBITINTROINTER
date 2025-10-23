4713cec04ace71df1711c100076fe0a9827297cc
HarryGrant34601 <hg4021@hw.ac.uk>
2025-10-23 12:25:15 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,17 @@
+let Score = 0
+function Start () {
+    music.stopAllSounds()
+    Score = 34
+    EndGame()
+}
+input.onButtonPressed(Button.AB, function () {
+    Start()
+})
+function EndGame () {
+    basic.showString("Your Score was" + Score)
+    music.setVolume(255)
+    music.play(music.tonePlayable(131, music.beat(BeatFraction.Whole)), music.PlaybackMode.LoopingInBackground)
+}
 basic.forever(function () {
 	
 })
